1a7ae763524cb4467881e546bf4271c7927d2988
latipiitech <latipiitech@gmail.com>
2026-05-18 18:39:15 +0100

    modal stuff done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import './components/modals/ModalButton'
 import './App.css'
 import ModalButton from './components/modals/ModalButton'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <div className='min-h-screen bg-blue-50 pt-10'>
-      <ModalButton />
+    <div className='min-h-screen bg-blue-50'>
+      <Navbar />
+      <div className="pt-10">
+        <ModalButton />
+      </div>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  const leftLinks = ['Agence', 'Activités', 'Projets', 'Actualités']
+  const rightLinks = ['Jobs', 'Marchés']
+
+  return (
+    <div>Navbar</div>
+  )
+}
